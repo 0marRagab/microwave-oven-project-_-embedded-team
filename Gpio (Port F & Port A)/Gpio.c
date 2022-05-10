b5cb6cf483536ff93aa60aee084021f587df8ba9
Mina-Gamal-210 <menagamal210@gmail.com>
2022-05-10 22:20:08 +0200

    Initialization of Port F and Port A

---
+++ Gpio (Port F & Port A)/Gpio.c
@@ -0,0 +1,15 @@
+#include "tm4c123gh6pm.h"
+#include "Gpio.h"
+
+
+void PortA_Init(){
+	SYSCTL_RCGCGPIO_R |=0x01;                          
+	while((SYSCTL_PRGPIO_R&0x01) == 0){}                                
+        GPIO_PORTA_CR_R=0x18;                         
+	GPIO_PORTA_AFSEL_R=0x00000000;                  
+	GPIO_PORTA_PCTL_R=0x00000000;                
+	GPIO_PORTA_AMSEL_R=0X00000000;                
+	GPIO_PORTA_DIR_R=0x08;                            
+	GPIO_PORTA_DEN_R=0xFF;                      
+	GPIO_PORTA_PUR_R=0x00000000;               
+}
