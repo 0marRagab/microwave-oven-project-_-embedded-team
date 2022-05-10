6588bf3fdbf405e57a87ade4ba055d4a32a28e40
Moustafa Yasser <94126494+Eng-DO4@users.noreply.github.com>
2022-05-10 23:44:47 +0200

    Update keypad.c

--- keypad/keypad.c
+++ keypad/keypad.c
@@ -1,1 +1,10 @@
+#include "tm4c123gh6pm.h"
+#include "keypad.h"
+#include "Std_INT.h"
+
+// adding array of charachter in keypad
+u8 symbol[N_row][N_col] = {{ '1', '2',  '3', 'A'},      // row 1
+                           { '4', '5',  '6', 'B'},      // row 2
+                           { '7', '8',  '9', 'C'},      // row 3
+                           { '*', '0',  '#', 'D'}};     // row 4
 
