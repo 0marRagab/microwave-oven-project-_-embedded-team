7c97e8e4f3c2d6458d9f3b96be31822616a69685
shimaa204 <104991119+shimaa204@users.noreply.github.com>
2022-05-11 18:27:14 +0200

    Update Gpio.c

--- Gpio (Port F & Port A)/Gpio.c
+++ Gpio (Port F & Port A)/Gpio.c
@@ -14,6 +14,19 @@ void PortF_Init(){
 	GPIO_PORTF_PUR_R=0x11;             
 }
 
+void PortD_Init(){
+	SYSCTL_RCGCGPIO_R |=0x10;                   
+	while((SYSCTL_PRGPIO_R&0x10) == 0){}                                
+	GPIO_PORTF_LOCK_R= 0x4C4F434B;                                       
+        GPIO_PORTF_CR_R=0xFF;                 
+	GPIO_PORTF_AFSEL_R=0x00000000;        
+	GPIO_PORTF_PCTL_R=0x00000000;          
+	GPIO_PORTF_AMSEL_R=0X00000000;        
+	GPIO_PORTF_DIR_R=0x20;                
+	GPIO_PORTF_DEN_R=0xFF;               
+	GPIO_PORTF_PUR_R=0x40;             
+}
+
 int sw1_in() {                                  
   char x = GPIO_PORTF_DATA_R & 0x10;
 	
