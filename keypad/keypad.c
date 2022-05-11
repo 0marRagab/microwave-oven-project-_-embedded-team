285503edeb5c491be49837c5f71f4510e16599e0
Moustafa Yasser <94126494+Eng-DO4@users.noreply.github.com>
2022-05-11 18:39:46 +0200

    Update keypad.c

--- keypad/keypad.c
+++ keypad/keypad.c
@@ -4,29 +4,37 @@
 #include "delay.h"
 
 // adding array of charachter in keypad
-u8 symbol[N_row][N_col] = {{ '1', '2',  '3', 'A'},      // row 1
-                           { '4', '5',  '6', 'B'},      // row 2
-                           { '7', '8',  '9', 'C'},      // row 3
-                           { '*', '0',  '#', 'D'}};     // row 4
+//			     col1  col2  col3  col4
+u8 symbol[N_row][N_col] = {{ '1',  '2',  '3',  'A'},  // row 1
+                           { '4',  '5',  '6',  'B'},  // row 2
+                           { '7',  '8',  '9',  'C'},  // row 3
+                           { '*',  '0',  '#',  'D'}}; // row 4
+/*
+ * We will use pins		        in Port E as inputs (keypad rows)
+ *		    PE4, PE3, PE2, PE1
+ *    x    x    x    0    0    0    0    x 			-->  & 11100001
+ *								-->  &= 0xE1
+ *
+ * We will use pins			in Port C as outputs (keypad columns)
+ *		    PC7, PC6, PC5, PC4
+ *		     1    1    1    1    x    x    x    x	-->  | 11110000
+ * 								-->  |= 0xF0
+ */
 
-
-// Keypad_Init() configures PORTC and PORTA to scan keypad keys
+// Keypad_Init() configures PORTC and PORTE to scan keypad keys
 void Keypad_Init(void)
-{
-  SYSCTL_RCGCGPIO_R |= 0x00000005;        //Enable clock for PORTA and PORTC  
-  while ((SYSCTL_PRGPIO_R&0x05) == 0){};  //Wait for clock to be setted
-	GPIO_PORTA_LOCK_R = 0x4C4F434B;
+{												// FE DCBA
+	SYSCTL_RCGCGPIO_R |= 0x14;			// enable clock for PORTC and PORTE       (01 0100) --> (0x14)
+	while ((SYSCTL_PRGPIO_R&0x14) == 0){};		// wait for clock to be setted
 	GPIO_PORTC_LOCK_R = 0x4C4F434B;
-  GPIO_PORTC_CR_R |= 0xF0;             //Allow settings for pins (4,5,6,7) of PORTC
-  GPIO_PORTA_CR_R |= 0x3C;             //Allow settings for pins (2,3,4,5) of PORTA
-	GPIO_PORTC_DIR_R |= 0xF0;             //PC4-PC7 are used with col and set them as digital output pins
-  GPIO_PORTA_DIR_R = 0x00;             //PA2-PA5 are used with row and set them as digital input pins
-  GPIO_PORTA_PDR_R = 0x3C;             //Enable pull down resistor on PORTA for rows
-  GPIO_PORTC_DEN_R |= 0xF0;
-  GPIO_PORTA_DEN_R = 0x3C;
-	GPIO_PORTA_AMSEL_R = 0x00;
-	GPIO_PORTA_AFSEL_R = 0x00;
-	GPIO_PORTA_PCTL_R = 0x00000000;
+	GPIO_PORTE_LOCK_R = 0x4C4F434B;
+	GPIO_PORTC_CR_R |= 0xF0;			// allow settings for pins (4,5,6,7) of PORTC	11110000 --> 0xF0
+  	GPIO_PORTE_CR_R |= 0x1E;			// allow settings for pins (1,2,3,4) of PORTE	00011110 --> 0x1E
+	GPIO_PORTC_DIR_R |= 0xF0;			// setting PC4-PC7 as output pins
+  	GPIO_PORTE_DIR_R &= 0xE1;			// setting PE2-PE5 as input pins
+  	GPIO_PORTA_PDR_R |= 0x1E;			// enable pull down resistor on PORTE for rows
+  	GPIO_PORTC_DEN_R |= 0xF0;
+  	GPIO_PORTA_DEN_R |= 0x1E;
 }
 
 //Keypad_getkey() function returns the value of key pressed by the user
