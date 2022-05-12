c429d849e195cb10bc8c78f636af73f2ea2986dd
omarnkaram771 <93954411+omarnkaram771@users.noreply.github.com>
2022-05-12 20:22:13 +0200

    Update LCD.c

--- LCD/LCD.c
+++ LCD/LCD.c
@@ -1,3 +1,18 @@
 #include "io.h"
 #include "LCD.h"
 #include "delay.h"
+
+//initilization port B for data
+void PortB_Init(void)
+{ 
+SYSCTL_RCGCGPIO_R |= 0x02;                //Activate the Clock on port B
+while ((SYSCTL_PRGPIO_R & 0x02) == 0){}; //waiting to until the clock is activated
+GPIO_PORTB_LOCK_R = 0x4C4F434B;         //Unlocking of port B
+GPIO_PORTB_CR_R |= 0xFF;               //to allow change in pin0--->pin7 in port B 
+GPIO_PORTB_DIR_R |= 0xFF;             //make pin0--->pin7 in port B as output
+GPIO_PORTB_DEN_R |= 0xFF;            //make pin0--->pin7 in port B as digital Pins 
+GPIO_PORTB_AMSEL_R &= 0;            //Disable analog signal register
+GPIO_PORTB_AFSEL_R &= 0;           //Disable alternate function register
+GPIO_PORTB_PCTL_R &= 0;           //make all pins as GPIO pins
+GPIO_PORTB_PUR_R = 0x00;         //disable pull up register
+}
