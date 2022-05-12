f777897341df5111b42b215a4d2fcc85c1580bf6
Youssef Emad <96674363+Yossef-Emad@users.noreply.github.com>
2022-05-12 22:20:03 +0200

    Update LCD.c

--- LCD/LCD.c
+++ LCD/LCD.c
@@ -31,3 +31,42 @@ GPIO_PORTD_AFSEL_R &= 0x00;                //disable alternate function register
 GPIO_PORTD_PCTL_R &= 0x00;                //make pin0--->pin7 in port D as GPIO
 GPIO_PORTD_PUR_R = 0x00;                 //disable pull up register
 }
+// Function used to make timer on LCD
+// call lcd_timer (char array of counter digits without coulmn ex to set timer for 2 min lcd_timer(0200) )
+void lcd_timer(u8* x){
+	char i,j,k,l;
+	char y = x[0];
+	char m = x[1];
+	char n = x[2];
+	char z = x[3];
+	lcd_setCursor(9,2);
+	lcd_write(':');
+	for(i=y ; i>='0' ; i--){
+		lcd_setCursor(7,2);
+		lcd_write(i);
+		for (j=m ; j>='0' ; j--){
+			lcd_setCursor(8,2);
+			lcd_write(j);
+			for (k=n ; k>='0' ; k--){
+				lcd_setCursor(10,2);
+				lcd_write(k);
+				for (l=z ; l>='0' ; l--){
+					lcd_setCursor(11,2);
+					lcd_write(l);
+					delay_ms(1000);
+				}
+				z='9';
+			}
+			n='5';
+		}
+		m='9';
+	}
+}
+
+//Interface to start the writing process at certain digit in the LCD, call lcd_goto(col,row) 
+void lcd_setCursor(u8 x,u8 y)    
+{
+	u8 firstcharadr[]={first_line1, first_line2}; // FirstCharAddress[0]=0x80  ,,,, FirstCharAddress[1]=0xC0 
+	lcd_cmd(firstcharadr[y-1] + x - 1);
+	delay_ms(50);
+}
