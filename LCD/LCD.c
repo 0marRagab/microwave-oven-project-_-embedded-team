c8a5f1128d2d6061178b1e1c6ead2fe562a8a32d
omarnkaram771 <93954411+omarnkaram771@users.noreply.github.com>
2022-05-12 20:23:24 +0200

    Update LCD.c

--- LCD/LCD.c
+++ LCD/LCD.c
@@ -16,3 +16,18 @@ GPIO_PORTB_AFSEL_R &= 0;           //Disable alternate function register
 GPIO_PORTB_PCTL_R &= 0;           //make all pins as GPIO pins
 GPIO_PORTB_PUR_R = 0x00;         //disable pull up register
 }
+
+// initilization port D for en,rs,rw
+void PortD_Init(void)
+{ 
+SYSCTL_RCGCGPIO_R |= 0x08;                        //Activate the Clock on port D
+while ((SYSCTL_PRGPIO_R&0x08) == 0){};           //waiting to make change in port D
+GPIO_PORTD_LOCK_R = 0x4C4F434B;                 //Unlocking of port D
+GPIO_PORTD_CR_R |= 0xFF;                       //to allow change in pin0--->pin7 in port D
+GPIO_PORTD_DIR_R |= 0xFF;                     //make pin0--->pin7 in port D as output
+GPIO_PORTD_DEN_R |= 0xFF;                    //make pin0--->pin7 in port D as digital Pins
+GPIO_PORTD_AMSEL_R &= 0x00;                 //Disable analog signal register
+GPIO_PORTD_AFSEL_R &= 0x00;                //disable alternate function register on port D
+GPIO_PORTD_PCTL_R &= 0x00;                //make pin0--->pin7 in port D as GPIO
+GPIO_PORTD_PUR_R = 0x00;                 //disable pull up register
+}
