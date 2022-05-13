9d95a647513441412cbe9e57c3ef7b3bd80ccb55
omarnkaram771 <93954411+omarnkaram771@users.noreply.github.com>
2022-05-13 21:15:09 +0200

    Update LCD.h

---
+++ LCD/LCD.h
@@ -0,0 +1,17 @@
+#ifndef LCD_H
+#define LCD_H
+#include "Std_INT.h"
+
+/* PIN Defines */
+#define PIN0   0
+#define PIN1   1
+#define PIN2   2
+#define PIN3   3
+#define PIN4   4
+#define PIN5   5
+#define PIN6   6
+#define PIN7   7
+
+/* PIN Value Options */
+#define HIGH   1
+#define LOW    0
