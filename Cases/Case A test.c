19f85c99a66ad68da65ea9f2a253a337ce74b525
omarnkaram771 <93954411+omarnkaram771@users.noreply.github.com>
2022-05-16 18:17:00 +0200

    Update Case A test.c

--- Cases/Case A test.c
+++ Cases/Case A test.c
@@ -3,9 +3,9 @@ case 'A':
 				lcd_setCursor(5,1);                    // call lcd_goto(col,row) to set cursor at specific location
 				lcd_print("Popcorn");
 				delay_ms(2000);
-			  lcd_clear();
+			        lcd_clear();
 				lcd_setCursor(1,1);
-		    lcd_print ("EN s2 to start");
+		                lcd_print ("EN s2 to start");
 				while(sw2_in()==0){}
 				lcd_clear();
 				lcd_setCursor(5,1);                    // call lcd_goto(col,row) to set cursor at specific location
