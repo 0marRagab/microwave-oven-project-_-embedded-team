f10ced418941e1f7f08d28e71f4b6fbbc7a37a4c
yasmeen abdelrazek <99843777+yasmeen-abdelrazek@users.noreply.github.com>
2022-05-17 09:17:19 +0200

    Update delay.c

--- Used Functions/delay.c
+++ Used Functions/delay.c
@@ -1,7 +1,11 @@
 #include "tm4c123gh6pm.h"
 #include "delay.h"
 
-// Initialization of SysTick Timer
+/*                                    ********************************
+************************************** ___SysTick_Init() function___  **************************************
+                                      ********************************
+Initialization of SysTick Timer
+*/
 void SysTick_Init(){
     NVIC_ST_CTRL_R=0;
     NVIC_ST_RELOAD_R=0x00FFFFFF;
@@ -10,8 +14,12 @@ void SysTick_Init(){
 }
 
 
-//to make delay one millisecond
-void delay_onems()	{
+/*                                    *******************************
+************************************** ___delay_onems() function___  **************************************
+                                      *******************************
+to make delay one millisecond
+*/
+void delay_onems(){
     NVIC_ST_CTRL_R=0;                                 // disable timer
     NVIC_ST_RELOAD_R=16000-1;                         // 16000 clock means 1 ms because the clock frequency is 16 MHz not 80 MHz
     NVIC_ST_CURRENT_R=0;                              // clear counter value
@@ -19,7 +27,12 @@ void delay_onems()	{
     while ((NVIC_ST_CTRL_R & 0X10000)==0){}           // loop untill count bit =1 which mean that timer complete counting
 }
 
-//to make delay one microsecond
+
+/*                                    *******************************
+************************************** ___delay_oneus() function___  **************************************
+                                      *******************************
+to make delay one microsecond
+*/
 void delay_oneus(){
     NVIC_ST_CTRL_R=0;                                     // disable timer
     NVIC_ST_RELOAD_R=16-1;                                // 16 clock means 1 us because the clock frequency is 16 MHz not 80 MHz
@@ -28,29 +41,42 @@ void delay_oneus(){
     while ((NVIC_ST_CTRL_R & 0X10000)==0){}               // loop untill count bit =1 which mean that timer complete counting  
 }
 
-// call delay_ms(*time*) and send number of milliseconds to delay (wait)
-void delay_ms(unsigned int x){
+
+/*                                    *****************************
+**************************************  ___delay_ms() function___  **************************************
+                                      *****************************
+call delay_ms(*time*) and send number of milliseconds to delay (wait)
+*/
+void delay_ms(unsigned int x){\
     int i;
-    for(i=0 ; i<x ; i++){
-        delay_onems();
-    }
-}
-
-
-// call delay_us(*time*) and send number of microseconds to delay (wait)
-void delay_us(unsigned int y){
-   int i;
-   for(i=0 ; i<y ; i++){
-       delay_oneus();
+   for(i=0 ; i<x ; i++){
+       delay_onems();
    }
 }
 
 
+/*                                    ****************************
+************************************** ___delay_us() function___  **************************************
+                                      ****************************
+call delay_us(*time*) and send number of microseconds to delay (wait)
+*/
+void delay_us(unsigned int y){
+    int i;
+    for(i=0 ; i<y ; i++){
+        delay_oneus();
+    }
+}
 
-int delay_portA_interrupt(unsigned int x){ 
+
+/*                                    ****************************************
+************************************** ___delay_portA_interrupt() function___ **************************************
+                                      ****************************************
+to make cheeking sw3 more guik in interrupt function                                       
+*/
+int delay_portA_interrupt(unsigned int x){
     int i;
     for(i=0 ; i<x ; i++){ 
-        delay_onems();
+        delay_onems(); 
         if ( sw3_in() ==0){
             return 0;
         }
