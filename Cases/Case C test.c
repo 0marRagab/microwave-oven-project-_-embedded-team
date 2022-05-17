f96d00af13976f55241ee47aa9d3c486a116dfe0
yasmeen abdelrazek <99843777+yasmeen-abdelrazek@users.noreply.github.com>
2022-05-17 08:11:42 +0200

    Update Case C test.c

--- Cases/Case C test.c
+++ Cases/Case C test.c
@@ -12,48 +12,42 @@ unsigned int mod(u32 num, u32 div){		// func to return remainder (modulo)
 }
 
 int main() {
-char key;	
-SysTick_Init();
-init_lcd();
-Keypad_Init();
-	
-	
-while(1) {
-	
-key = Keypad_Getkey();	
-	
-switch(key)	
-case 'C':
-	
-	while(i){
-		lcd_clear();
-		lcd_setCursor(1,1);
-	    	lcd_print("Chicken weight?");
-	    	lcd_setCursor(1,2);
-		lcd_print("weight:");
-		index=0;
-		lcd_setCursor(8,2);
-		while(sw2_in()==0){
-			c = keypad_getkey_caseD();
-			delay_ms(200);
-			if(c!='z'){
-				lcd_write(c);
-				u[index]=c;
-				index++;
-			}
-		}
-		if (index==1){
-			// put Ur code here
-			
-		}else{
-			lcd_setCursor(8,2);
-			lcd_print("       ");
-			lcd_setCursor(8,2);
-			lcd_print("Err");
-			delay_ms(2000);
+	char key;
+	SysTick_Init();
+	init_lcd();
+	Keypad_Init();
+	while(1) {
+		key = Keypad_Getkey();	
+		switch(key){	
+			case 'C':
+				while(i){
+					lcd_clear();
+					lcd_setCursor(1,1);
+					lcd_print("Chicken weight?");
+					lcd_setCursor(1,2);
+					lcd_print("weight:");
+					index=0;
+					lcd_setCursor(8,2);
+					while(sw2_in()==0){
+						c = keypad_getkey_caseD();
+						delay_ms(200);
+						if(c!='z'){
+							lcd_write(c);
+							u[index]=c;
+							index++;
+						}
+					}
+					if (index==1){
+						// put Ur code here
+					}
+					else{
+						lcd_setCursor(8,2);
+						lcd_print("       ");
+						lcd_setCursor(8,2);
+						lcd_print("Err");
+						delay_ms(2000);
+					}
+				}
+				break;
 		}
 	}
-	
-	break;
-}
-}
