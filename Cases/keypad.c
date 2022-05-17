b81b8a924070c51535f22e211d33debd0c2cc4bb
yasmeen abdelrazek <99843777+yasmeen-abdelrazek@users.noreply.github.com>
2022-05-17 09:04:05 +0200

    Update keypad.c

--- Cases/keypad.c
+++ Cases/keypad.c
@@ -22,8 +22,7 @@ u8 symbol[N_row][N_col] = {{ '1',  '2',  '3',  'A'},  // row 1
  */
 
 // Keypad_Init() configures PORTC and PORTE to scan keypad keys
-void Keypad_Init(void)
-{												// FE DCBA
+void Keypad_Init(void){												// FE DCBA
 	SYSCTL_RCGCGPIO_R |= 0x14;			// enable clock for PORTC and PORTE       (01 0100) --> (0x14)
 	while ((SYSCTL_PRGPIO_R&0x14) == 0){};		// wait for clock to be setted
 	GPIO_PORTC_LOCK_R = 0x4C4F434B;
@@ -38,42 +37,35 @@ void Keypad_Init(void)
 }
 
 //keypad_getkey() is a function that returns the value of the pressed key by the user.
-u8 keypad_getkey(void)
-{
+u8 keypad_getkey(void){
 	int i;
 	int j;
-  while(1)
-  {
-    for(i = 0; i < 4; i++)                        
-    {
-      GPIO_PORTC_DATA_R = (1U << i+4);
-      delay_us(2);
-      for(j = 0; j < 4; j++)                     
-      {
-        if((GPIO_PORTE_DATA_R & 0x1E) & (1U << j+1))
-          return symbol[j][i];
-      }
-    }
-  }
+	while(1){
+		for(i = 0; i < 4; i++){
+			GPIO_PORTC_DATA_R = (1U << i+4);
+			delay_us(2);
+			for(j = 0; j < 4; j++){
+				if((GPIO_PORTE_DATA_R & 0x1E) & (1U << j+1))
+					return symbol[j][i];
+			}
+		}
+	}
 }
 
+
 // Function used to get number from user in case d
-char keypad_getkey_caseD(void)
-{
+char keypad_getkey_caseD(void){
 	int i;
 	int j;
-  while(sw2_in()==0 & sw1_in()==0)
-  {
-    for(i = 0; i < 4; i++)                        //columns traverse
-    {
-      GPIO_PORTC_DATA_R = (1U << i+4);
-      delay_us(2);
-      for(j = 0; j < 4; j++)                     //raws traverse
-      {
-        if((GPIO_PORTE_DATA_R & 0x1E) & (1U << j+1))
-          return symbol[j][i];
-      }
-    }
-  }
+	while(sw2_in()==0 & sw1_in()==0){
+		for(i = 0; i < 4; i++){                        //columns traverse
+			GPIO_PORTC_DATA_R = (1U << i+4);
+			delay_us(2);
+			for(j = 0; j < 4; j++){                     //raws traverse
+				if((GPIO_PORTE_DATA_R & 0x1E) & (1U << j+1))
+					return symbol[j][i];
+			}
+		}
+	}
 	return 'z';
 }
