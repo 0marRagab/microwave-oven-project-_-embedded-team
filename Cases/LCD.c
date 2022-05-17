1a3b37ab11fec6aa600cbf8627c189d1090cbdda
yasmeen abdelrazek <99843777+yasmeen-abdelrazek@users.noreply.github.com>
2022-05-17 08:45:30 +0200

    Update LCD.c

--- Cases/LCD.c
+++ Cases/LCD.c
@@ -73,9 +73,8 @@ return;
                                       *****************************
  to clear lcd 
 */
-void lcd_clear()
-{
-lcd_cmd(lcd_Clear);	
+void lcd_clear(){
+lcd_cmd(lcd_Clear);
 }
 
 /*                                    ***************************
@@ -83,15 +82,13 @@ lcd_cmd(lcd_Clear);
                                       ***************************
 Interface to send the configuration commands to the LCD Driver
 */
-void lcd_cmd(u8 cmd)            
-{
-GPIO_PORTB_DATA_R=cmd;	
-GPIO_PORTD_DATA_R=(LOW<<PIN2)|(LOW<<PIN3)|(HIGH<<PIN6);
-delay_ms(1);
-GPIO_PORTD_DATA_R=(LOW<<PIN2)|(LOW<<PIN3)|(LOW<<PIN6);
-delay_ms(50);
-	
-return;
+void lcd_cmd(u8 cmd){
+	GPIO_PORTB_DATA_R=cmd;	
+	GPIO_PORTD_DATA_R=(LOW<<PIN2)|(LOW<<PIN3)|(HIGH<<PIN6);
+	delay_ms(1);
+	GPIO_PORTD_DATA_R=(LOW<<PIN2)|(LOW<<PIN3)|(LOW<<PIN6);
+	delay_ms(50);
+	return;
 }
 
 /*                                    *********************************
@@ -99,11 +96,10 @@ return;
                                       *********************************
 Interface to start the writing process at certain digit in the LCD, call lcd_goto(col,row) 
 */
-void lcd_setCursor(u8 x,u8 y)    
-{
-u8 firstcharadr[]={first_line1, first_line2}; // FirstCharAddress[0]=0x80  ,,,, FirstCharAddress[1]=0xC0 
-lcd_cmd(firstcharadr[y-1] + x - 1);
-delay_ms(50);
+void lcd_setCursor(u8 x,u8 y){
+	u8 firstcharadr[]={first_line1, first_line2}; // FirstCharAddress[0]=0x80  ,,,, FirstCharAddress[1]=0xC0 
+	lcd_cmd(firstcharadr[y-1] + x - 1);
+	delay_ms(50);
 }
 
 /*                                    ******************************
@@ -111,15 +107,13 @@ delay_ms(50);
                                       ******************************
 Interface to write character on LCD screen
 */
-void lcd_write(u8 data)
-{
-GPIO_PORTB_DATA_R=data;	
-GPIO_PORTD_DATA_R=(HIGH<<PIN2)|(LOW<<PIN3)|(HIGH<<PIN6);
-delay_ms(1);
-GPIO_PORTD_DATA_R=(HIGH<<PIN2)|(LOW<<PIN3)|(LOW<<PIN6);
-delay_ms(50);
-	
-return;
+void lcd_write(u8 data){
+	GPIO_PORTB_DATA_R=data;	
+	GPIO_PORTD_DATA_R=(HIGH<<PIN2)|(LOW<<PIN3)|(HIGH<<PIN6);
+	delay_ms(1);
+	GPIO_PORTD_DATA_R=(HIGH<<PIN2)|(LOW<<PIN3)|(LOW<<PIN6);
+	delay_ms(50);
+	return;
 }
 
 /*                                    ******************************
@@ -127,16 +121,14 @@ return;
                                       ******************************
 Interface to write string on LCD screen
 */
-void lcd_print(u8 *str)  
-{
-int		i=0;
- while(str[i]!='\0')
- {
-  lcd_write(str[i]);
-  i++;	
- }
-delay_ms(100);
-return;	
+void lcd_print(u8 *str){
+	int i=0;
+	while(str[i]!='\0'){
+		lcd_write(str[i]);
+		i++;	
+	}
+	delay_ms(100);
+	return;	
 }
 
 /*                                    ******************************
@@ -146,13 +138,11 @@ return;
 */				      
 void lcd_string(u8 str1,u8 str2,u8 str3,u8 str4,u8 str5)  //printing concatenating charcters on screen
 {
-lcd_write(str1);
-lcd_write(str2);
-lcd_write(str3);
-lcd_write(str4);
-lcd_write(str5);
-delay_ms(100);
-	
-return;
-	
+	lcd_write(str1);
+	lcd_write(str2);
+	lcd_write(str3);
+	lcd_write(str4);
+	lcd_write(str5);
+	delay_ms(100);
+	return;
 }
