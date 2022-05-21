a5f45ed1e574e13db9e55b4c45b2d1bf1ff245b4
yasmeen abdelrazek <99843777+yasmeen-abdelrazek@users.noreply.github.com>
2022-05-21 23:23:09 +0200

    Update main.c

--- main.c
+++ main.c
@@ -113,6 +113,58 @@ int main(){
 				break;
 				
 				
+			case beef_state:
+				time_arr[0]='0'; time_arr[3]='0'; 		// time always be 0x:x0 
+				while(i){
+					lcd_clear();
+					lcd_setCursor(1,1);
+					lcd_print("Beef weight?");
+					lcd_setCursor(1,2);
+					lcd_print("weight:");
+					index=0;
+					lcd_setCursor(8,2);
+					while(sw2_in()==0){
+						c = keypad_getkey_caseD();
+						delay_ms(200);
+						if(c!='z'){
+						lcd_write(c);
+						u[index]=c;
+						index++;
+						}
+					}
+					i=0 ; 	
+					previous_state = beef_state;
+					state = cooking_state;
+				}
+				break;
+			
+			
+			case chicken_state:
+				time_arr[0]='0'; 		// time always be 0x:xx 
+				while(i){
+					lcd_clear();
+					lcd_setCursor(1,1);
+					lcd_print("Chicken weight?");
+					lcd_setCursor(1,2);
+					lcd_print("weight:");
+					index=0;
+					lcd_setCursor(8,2);
+					while(sw2_in()==0){
+						c = keypad_getkey_caseD();
+						delay_ms(200);
+						if(c!='z'){
+						lcd_write(c);
+						u[index]=c;
+						index++;
+						}
+					}
+					i = 0;
+					previous_state = chicken_state;
+					state = cooking_state;
+				}
+				break;
+				
+				
 			case done_state:
 				lcd_clear();
 				lcd_setCursor(1,1);
