29d4f8521535645373a1545e339009bd0a65362d
yasmeen abdelrazek <99843777+yasmeen-abdelrazek@users.noreply.github.com>
2022-05-21 06:07:46 +0200

    Update Functions.h

--- Used Functions/Functions.h
+++ Used Functions/Functions.h
@@ -12,10 +12,9 @@ void RGB_BLINK();
 // call RGB_FlASH() to Flash the RGB array 3 times
 void RGB_FLASH();    
 
+enum states { initial_state = 1 , choosing_state , popcorn_state, beef_state, chicken_state, setTime_state, cooking_state, puase_state, stop_state, done_state } ;
 int super_timer(u8 min,u8 mmin,u8 sec,u8 msec);
                          
-
-
 
 #endif
 
