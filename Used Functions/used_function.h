59d6548a19594d54aef2cece48e496cbc5a711e4
omarnkaram771 <93954411+omarnkaram771@users.noreply.github.com>
2022-05-24 01:24:11 +0200

    Update used_function.h

--- Used Functions/used_function.h
+++ Used Functions/used_function.h
@@ -11,20 +11,30 @@ extern char temp;
 
 
 enum states { initial_state = 1 , choosing_state , popcorn_state, beef_state, chicken_state, setTime_state, cooking_state, pause_state, stop_state, done_state } ;
+
+
+//
 int super_timer(u8 min,u8 mmin,u8 sec,u8 msec);
                          
 
 // call RGB_BLINK() to blink the RGB array
 void RGB_BLINK();                           
   
-  
+
 // call RGB_FlASH() to Flash the RGB array 3 times
 void RGB_FLASH();    
 
 
+//to print error msg if unexpected button is pressed
 void Error_msg1();
 
+
+//to print error msg if unexpected button is pressed
 void Error_msg2();
+
+
+//func to return remainder (modulo)
+unsigned int mod(u32 num, u32 div);
 
 
 #endif
