cd0a4fd51b33183dad4f7376351737e59801efe4
yasmeen abdelrazek <99843777+yasmeen-abdelrazek@users.noreply.github.com>
2022-05-23 05:54:07 +0200

    Update Functions.c

--- Used Functions/Functions.c
+++ Used Functions/Functions.c
@@ -101,7 +101,6 @@ call super_timer to display timer on screen
 */
 int super_timer(u8 min,u8 mmin,u8 sec,u8 msec){
 	int i;
-	
 	if( (min<='9' && min>='0') && (mmin<='9' && mmin>='0') && (sec<='9' && sec>='0') && (msec<='9' && msec>='0')){
 		if(mmin == '9' && sec > '5'){
 			min++;
@@ -114,32 +113,30 @@ int super_timer(u8 min,u8 mmin,u8 sec,u8 msec){
 		}
 		if(min>'3'||(min=='3'&&mmin>'0')||(min=='3'&&mmin=='0'&&sec>'0')||(min=='3'&&mmin=='0'&&sec=='0'&&msec>'0')){
 			RGB_OFF();
-		  lcd_setCursor(2,2);
-		  lcd_print("ErrMax=30:00");
-		  delay_ms(1500);
-	    lcd_clear();
-	    return 0;
+			lcd_setCursor(2,2);
+		  	lcd_print("ErrMax=30:00");
+		  	Buzzer_ON();
+			delay_ms(1500);
+			Buzzer_OFF();
+	    		lcd_clear();
+	    		return 0;
+		}
 	}
-}
 	else {
 		RGB_OFF();
-		lcd_setCursor(6,2);
-		lcd_print("Error");
-		delay_ms(1500);
+		Error_msg1();
 		lcd_clear();
 		lcd_print("Enter bet 1:9");
 		delay_ms(1500);
-	  lcd_clear();
-	  return 0;
+	  	lcd_clear();
+	  	return 0;
 	}
 	while(1){
-		
-if (sw1_in()==1){
-	delay_ms(400);
-	O[0]=min ;O[1]= mmin ;O[2]=sec ;O[3]=msec ;
-	return 2;
-}
-else {		
+		if (sw1_in()==1){
+			delay_ms(400);
+			O[0]=min ;O[1]= mmin ;O[2]=sec ;O[3]=msec ;
+			return 2;
+		}
 		RGB_ON();
 		lcd_setCursor(6,2);
 		lcd_string(min,mmin,':',sec,msec);
@@ -149,8 +146,8 @@ else {
 		}
 		if(msec=='0'&&sec=='1'){
 			lcd_setCursor(6,2);
-    	lcd_string(min,mmin,':',sec,msec);
-	  	delay_ms(1000);
+    			lcd_string(min,mmin,':',sec,msec);
+  			delay_ms(1000);
 		}
 		if(msec=='0'&&sec!='0'){
 			msec='9';
@@ -173,82 +170,5 @@ else {
 			 break;
 		}
 	}
-}
-return 1;
-}int super_timer(u8 min,u8 mmin,u8 sec,u8 msec){
-	int i;
-	
-	if( (min<='9' && min>='0') && (mmin<='9' && mmin>='0') && (sec<='9' && sec>='0') && (msec<='9' && msec>='0')){
-		if(mmin == '9' && sec > '5'){
-			min++;
-			mmin='0';
-			sec = sec - 6;
-		}
-		if(sec>'5'){
-		mmin+=1;
-		sec = sec - 6;
-		}
-		if(min>'3'||(min=='3'&&mmin>'0')||(min=='3'&&mmin=='0'&&sec>'0')||(min=='3'&&mmin=='0'&&sec=='0'&&msec>'0')){
-			RGB_OFF();
-		  lcd_setCursor(2,2);
-		  lcd_print("ErrMax=30:00");
-		  delay_ms(1500);
-	    lcd_clear();
-	    return 0;
-	}
-}
-	else {
-		RGB_OFF();
-		lcd_setCursor(6,2);
-		lcd_print("Error");
-		delay_ms(1500);
-		lcd_clear();
-		lcd_print("Enter bet 1:9");
-		delay_ms(1500);
-	  lcd_clear();
-	  return 0;
-	}
-	while(1){
-		
-if (sw1_in()==1){
-	delay_ms(400);
-	O[0]=min ;O[1]= mmin ;O[2]=sec ;O[3]=msec ;
-	return 2;
-}
-else {		
-		RGB_ON();
-		lcd_setCursor(6,2);
-		lcd_string(min,mmin,':',sec,msec);
-		delay_ms(1000);
-		if(msec!='0'){
-			msec--;
-		}
-		if(msec=='0'&&sec=='1'){
-			lcd_setCursor(6,2);
-    	lcd_string(min,mmin,':',sec,msec);
-	  	delay_ms(1000);
-		}
-		if(msec=='0'&&sec!='0'){
-			msec='9';
-			sec--;
-		}
-		if(msec=='0'&&sec=='0'&&mmin!='0'){
-			mmin--;
-			sec='5';
-			msec='9';
-		}
-		if(msec=='0'&&sec=='0'&&mmin=='0'&&min!='0'){
-			min--;
-			mmin='9';
-			sec='5';
-			msec='9';
-		}
-		if(msec=='0'&&sec=='0'&&min=='0'&&mmin=='0'){
-			lcd_setCursor(6,2);
-			lcd_string('0','0',':','0','0');
-			 break;
-		}
-	}
-}
-return 1;
+	return 1;
 }
