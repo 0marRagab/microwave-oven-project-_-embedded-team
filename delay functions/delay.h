ac5f806adf9223a0102c8c52aa099af4586c98fb
0marRagab <104281985+0marRagab@users.noreply.github.com>
2022-05-25 16:01:24 +0200

    Update delay.h

--- delay functions/delay.h
+++ delay functions/delay.h
@@ -17,6 +17,9 @@ void delay_ms(unsigned int x);
 // call delay_us(*time*)
 void delay_us(unsigned int y);
 
+void delay_o();
+*/
+int delay_m_s(unsigned int x);
 
 // Function to stop count down when switch is pressed
 int delay_portA_interrupt(unsigned int x);
